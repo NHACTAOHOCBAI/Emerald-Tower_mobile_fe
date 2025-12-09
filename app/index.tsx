249a676b432ee043fb3bfb54686acda005eef193
phuc nguyen <dangphucnguyen20112005@gmail.com>
2025-12-09 17:01:52 +0700

    base project with native wind

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+import "../global.css";
+const Home = () => {
+  return (
+    <View className="mt-10">
+      <Text className="text-blue-50">hi</Text>
+      <Text className="text-blue-50">hi</Text>
+      <Text className="text-blue-50">hi</Text>
+
+      <Text className="text-blue-50">hi</Text>
+      <Text>hi</Text>
+      <Text>hi</Text>
+      <Text>hi</Text>
+    </View>
+  );
+};
+export default Home;
